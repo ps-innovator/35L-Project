d5a6e65f3b1928892e5b389682924fa30450b438
JohnConnellCS <johnconnellcs@gmail.com>
2024-05-20 20:02:18 -0700

    Basic Sign Up Page Created, May delete non-SSO option later

--- uclax-frontend/src/pages/SignUp.jsx
+++ uclax-frontend/src/pages/SignUp.jsx
@@ -2,8 +2,41 @@ import { useState } from 'react';
 
 const SignUp = () => {
     return (
-        <>
-        </>
+        <div>
+        <h1
+        className="text-2xl text-black dark:text-white"
+        style={{
+            marginTop: "5%",
+            marginBottom: "5%",
+            fontWeight: "500",
+            fontSize: 45,
+        }}
+        >
+        Create An Account
+        </h1>
+          <div class="flex mb-6 items-center justify-center">
+            <button className="block text-white p-4 rounded-lg bg-indigo-500 dark:bg-slate-500 hover:bg-indigo-600 dark:hover:bg-slate-600 active:bg-indigo-900 dark:active:bg-slate-900" style={{fontSize: 25, fontWeight: 200}}> Sign in with UCLA SSO  </button>
+          </div>
+          <div className="flex items-center justify-center mb-6">
+            <span className="px-3 text-gray-500 dark:text-gray-400">OR</span>
+          </div>
+          <div class="flex mb-6 items-center justify-center">
+            <input name="uname"
+            placeholder="Username" 
+            className="block w-1/2 p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+          <div class="flex mb-6 items-center justify-center">
+            <input name="pwd"
+            placeholder="Password"
+            type="password"
+            className="block w-1/2 p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+          <div class="flex mb-6 items-center justify-center">
+            <button className="block text-white p-4 rounded-lg bg-indigo-500 dark:bg-slate-500 hover:bg-indigo-600 dark:hover:bg-slate-600 active:bg-indigo-900 dark:active:bg-slate-900" style={{fontSize: 25, fontWeight: 200}}> Sign up </button>
+          </div>
+        </div>
     )
 };
 
