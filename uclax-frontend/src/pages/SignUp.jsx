4085be73fa276a76cdfc9b790648839ca09446a8
JohnConnellCS <johnconnellcs@gmail.com>
2024-06-01 12:19:25 -0700

    Updated SignUp.jsx to not conflict with authentication put in place on main branch.

--- uclax-frontend/src/pages/SignUp.jsx
+++ uclax-frontend/src/pages/SignUp.jsx
@@ -1,40 +1,59 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { AuthContext, TabContext } from '../App';
 
 const SignUp = () => {
+    const { auth, setAuth } = useContext(AuthContext);
+    const { tab, setTab } = useContext(TabContext);
+    const [uname, setUname] = useState('');
+    const [pwd, setPwd] = useState('');
+    const attemptSignUp = async () => {
+        const res = await fetch('http://localhost:3000/auth/createaccount', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify({username: uname, password: pwd})
+        });
+        if(res.ok) {
+            const json = await res.json();
+            setAuth(json);
+            setTab(2);
+            console.log("works");
+        } else {
+            console.log("signup failed");
+        }
+    };
     return (
         <div>
-        <h1
-        className="text-2xl text-black dark:text-white"
-        style={{
-            marginTop: "5%",
-            marginBottom: "5%",
-            fontWeight: "500",
-            fontSize: 45,
-        }}
-        >
-        Create An Account
-        </h1>
-          <div class="flex mb-6 items-center justify-center">
-            <button className="block text-white p-4 rounded-lg bg-indigo-500 dark:bg-slate-500 hover:bg-indigo-600 dark:hover:bg-slate-600 active:bg-indigo-900 dark:active:bg-slate-900" style={{fontSize: 25, fontWeight: 200}}> Sign in with UCLA SSO  </button>
-          </div>
-          <div className="flex items-center justify-center mb-6">
-            <span className="px-3 text-gray-500 dark:text-gray-400">OR</span>
-          </div>
-          <div class="flex mb-6 items-center justify-center">
+          <h1
+            className="text-2xl text-black dark:text-white"
+            style={{
+              marginTop: "5%",
+              marginBottom: "5%",
+              fontWeight: "500",
+              fontSize: 45,
+            }}
+          >
+            Sign Up
+          </h1>
+          <div className="flex mb-6 items-center justify-center">
             <input name="uname"
             placeholder="Username" 
+            value={uname}
+            onChange={(e) => setUname(e.target.value)}
             className="block w-1/2 p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
           </div>
-          <div class="flex mb-6 items-center justify-center">
+          <div className="flex mb-6 items-center justify-center">
             <input name="pwd"
             placeholder="Password"
             type="password"
+            value={pwd}
+            onChange={(e) => setPwd(e.target.value)}
             className="block w-1/2 p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
           </div>
-          <div class="flex mb-6 items-center justify-center">
-            <button className="block text-white p-4 rounded-lg bg-indigo-500 dark:bg-slate-500 hover:bg-indigo-600 dark:hover:bg-slate-600 active:bg-indigo-900 dark:active:bg-slate-900" style={{fontSize: 25, fontWeight: 200}}> Sign up </button>
+          <div className="flex mb-6 items-center justify-center">
+            <button onClick={attemptSignUp} className="block text-white p-4 rounded-lg bg-indigo-500 dark:bg-slate-500 hover:bg-indigo-600 dark:hover:bg-slate-600 active:bg-indigo-900 dark:active:bg-slate-900" style={{fontSize: 25, fontWeight: 200}}>Sign Up</button>
           </div>
         </div>
     )
