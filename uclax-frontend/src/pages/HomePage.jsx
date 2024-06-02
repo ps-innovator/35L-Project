c9e39fab838d4c0c5709763c7caa0a8b6e76b64f
JohnConnellCS <johnconnellcs@gmail.com>
2024-06-02 13:56:21 -0700

    Added SignUp and LogIn links at bottom of HomePage in addition to new useState to deal with tab changes

--- uclax-frontend/src/pages/HomePage.jsx
+++ uclax-frontend/src/pages/HomePage.jsx
@@ -1,13 +1,23 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { AuthContext, TabContext } from "../App.jsx";
 import uclaxLogo from "/uclaxLogo.png";
 import CardView from "../components/CardView.jsx";
+import SignUp from "../pages/SignUp.jsx";
+import Login from "../pages/Login.jsx";
 
 const HomePage = () => {
   const { auth, setAuth } = useContext(AuthContext);
   const { tab, setTab } = useContext(TabContext);
+  const [view, setView] = useState("home");
 
   if (!auth) {
+
+    if (view === "login") {
+      setTab(2);
+    } else if (view === "signup") {
+      setTab(3);
+    }
+
     return (
       <>
         <div>
@@ -24,11 +34,23 @@ const HomePage = () => {
             </h1>
           <CardView
             header=""
-            shortDescr="Log in to view Posts"
+            shortDescr="Log In to View Posts"
             longDescr=""
             imgsrc={uclaxLogo}
             imgalt="Image of a plane whose trail is a spiral road. Caption is UCLAX."
           />
+          <button
+              onClick={() => setView("login")}
+              className="text-lg leading-tight font-medium text-black dark:text-zinc-400 hover:underline block"
+            >
+              Returning User? Log In
+            </button>
+            <button
+              onClick={() => setView("signup")}
+              className="text-lg leading-tight font-medium text-black dark:text-zinc-400 hover:underline block"
+            >
+              New User? Sign Up
+            </button>
         </div>
       </>
     );
