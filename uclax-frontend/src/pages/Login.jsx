550cb6febdc0db3824e89c9a02944895973c4243
pboreddy <pranayb325@gmail.com>
2024-06-05 01:25:16 -0700

    improved login page styling

--- uclax-frontend/src/pages/Login.jsx
+++ uclax-frontend/src/pages/Login.jsx
@@ -27,15 +27,7 @@ const Login = () => {
 
     return (
         <div>
-          <h1
-            className="text-2xl text-black dark:text-white"
-            style={{
-              marginTop: "5%",
-              marginBottom: "5%",
-              fontWeight: "500",
-              fontSize: 45,
-            }}
-          >
+          <h1 className="text-5xl text-black dark:text-white my-[5%] font-medium">
             Login
           </h1>
           <div className="flex mb-6 items-center justify-center">
@@ -43,7 +35,7 @@ const Login = () => {
             placeholder="Username" 
             value={uname}
             onChange={(e) => setUname(e.target.value)}   
-            className="block w-1/2 p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block w-[30%] p-4 text-gray-900 border-b border-gray-300 bg-white focus:outline-none dark:bg-slate-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
           </div>
           <div className="flex mb-6 items-center justify-center">
@@ -52,11 +44,11 @@ const Login = () => {
             value={pwd}
             onChange={(e) => setPwd(e.target.value)}
             type="password"
-            className="block w-1/2 p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block w-[30%] p-4 text-gray-900 border-b border-gray-300 bg-white focus:outline-none dark:bg-slate-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
           </div>
           <div className="flex mb-6 items-center justify-center">
-            <button onClick={attemptLogin} className="block text-white p-4 rounded-lg bg-indigo-500 dark:bg-slate-500 hover:bg-indigo-600 dark:hover:bg-slate-600 active:bg-indigo-900 dark:active:bg-slate-900" style={{fontSize: 25, fontWeight: 200}}>Log in</button>
+            <button onClick={attemptLogin} className=" text-white p-4 h-12 flex items-center justify-center rounded-lg bg-indigo-500 dark:bg-slate-500 hover:bg-indigo-600 dark:hover:bg-slate-600 active:bg-indigo-900 dark:active:bg-slate-900 text-xl font-extralight">Log in</button>
           </div>
         </div>
     )
